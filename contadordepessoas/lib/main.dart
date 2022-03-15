e0e0572f5fa793ef72a5a8d54ec21952a8ff70c3
RicMerces <ricardoliveira0703@gmail.com>
2022-03-14 22:43:37 -0300

    Parte 02

--- contadordepessoas/lib/main.dart
+++ contadordepessoas/lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-@override
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.purple,
-        child: Center(
-          child: const Text(
-            'Hello World !!',
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink,
+      alignment: Alignment.center,
+      child: const Text('Ola Mundo'),
     );
   }
 }
