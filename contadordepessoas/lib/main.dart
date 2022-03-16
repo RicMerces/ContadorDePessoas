9c19fe8e419773ef047d0e9b86f50decb25e403d
RicMerces <ricardoliveira0703@gmail.com>
2022-03-16 14:23:20 -0300

    Assets

--- contadordepessoas/lib/main.dart
+++ contadordepessoas/lib/main.dart
@@ -30,51 +30,78 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Images/image.jpg'),
           ),
-          const Text(
-            '0',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 100,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(color: Colors.black, fontSize: 24),
-                ),
-                style: TextButton.styleFrom(backgroundColor: Colors.white60),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pode entrar',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
-              TextButton(
-                onPressed: increment,
-                child: const Text(
-                  'Entrou',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
+            ),
+
+            // const SizedBox(height: 30),
+
+            const Padding(
+              padding: EdgeInsets.all(30),
+              child: Text(
+                '0',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 100,
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(color: Colors.black, fontSize: 24),
                   ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white60,
+                      fixedSize: const Size(100, 100),
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )),
                 ),
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white60,
-                    padding: const EdgeInsets.all(30)),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(
+                  width: 30,
+                ),
+                TextButton(
+                  onPressed: increment,
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white60,
+                      fixedSize: const Size(100, 100),
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
